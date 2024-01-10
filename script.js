da42f9c7e987789a072cd835090f6893ba93f96f
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-01-10 14:08:14 +0100

    activated submit button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+"use strict"
 // collecting DOM data
-
 const personalSection = document.getElementById("personal")
 const companySection = document.getElementById("company")
 const personalForm = document.getElementById("personal-form")
@@ -10,14 +10,19 @@ const personalPassword = document.getElementById("personal-password")
 const companyEmail = document.getElementById("company-email")
 const companyName = document.getElementById("company-name")
 const companyPassword = document.getElementById("company-password")
-const gender = document.querySelector(".radios > div > *")
+const gender = document.querySelector("input[name=gender]:checked")
+const personalSubmit = document.getElementById("personal-submit")
 
 const capitals = document.getElementById("capitals")
 const lowers = document.getElementById("lowers")
 const number = document.getElementById("number")
 const specials = document.getElementById("specials")
 const length = document.getElementById("length")
+var emailFlag = 0
+var passwordFlag = 0
+var nameFlag = 0
 
+const flagArray = []
 const hide = (element) =>{
     element.classList.add("hide")
 }
@@ -42,30 +47,37 @@ const inputStatusMessage = (element, message, success, failure) => {
         element.innerText = success
         element.classList.add("success")
         element.classList.remove("failure")
+        return 0
     }else{
         element.innerText = failure
         element.classList.add("failure")
         element.classList.remove("success")
+        return 1
     }
 }
 
 const emailChecker = (email) =>{
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,4}$/
-    return message = emailRegex.test(email) ? 1 : 0
+    const message = emailRegex.test(email) ? 1 : 0
+    return message
 }
 const nameChecker = (name) =>{
     const nameRegex = /^[a-z]+[a-z_' -]{4,29}$/i
-    return message = nameRegex.test(name) ? 1 : 0
+    const message = nameRegex.test(name) ? 1 : 0
+    return message
 }
 
 const passwordChecker = (password) => {
+    let innerPass = 0
     const passwordCases = (element, regex) =>{
         if(regex.test(password)){
             element.classList.add("success")
             element.classList.remove("failure")
+            innerPass = 1
         }else{
             element.classList.add("failure")
             element.classList.remove("success")
+            innerPass = 0
         }
     }
     // for upper cases
@@ -80,22 +92,39 @@ const passwordChecker = (password) => {
     // for specials
     passwordCases(specials, /[\@\$\+\/\_\.]/)
     // for length
-    if (password.length >= 8) {
+    if (innerPass === 1 && password.length >= 8) {
         length.classList.remove("failure")
         length.classList.add("success")
+        passwordFlag = 1
     }else{
         length.classList.remove("success")
         length.classList.add("failure")
+        innerPass = 0
+        passwordFlag = 0
+    }
+}
+const disableSubmit = () => {
+    if (emailFlag == 1 && nameFlag == 1 && passwordFlag == 1 && gender.value != null){
+        personalSubmit.removeAttribute("disabled")
+        personalSubmit.classList.remove("disabled")
+    }else{
+        personalSubmit.setAttribute("disabled", "")
+        personalSubmit.classList.add("disabled")
     }
 }
 personalEmail.addEventListener("input", () =>{
     const element = document.querySelector("#personal-email ~ p")
-    inputStatusMessage(element, emailChecker(personalEmail.value), "valid email", "invalid email")
+    if(!inputStatusMessage(element, emailChecker(personalEmail.value), "valid email", "invalid email")){
+        emailFlag = 1
+    }
+    disableSubmit()
 })
 personalName.addEventListener("input", () =>{
-    //event.preventDefault()
     const element = document.querySelector("#personal-name ~ p")
-    inputStatusMessage(element, nameChecker(personalName.value), "valid name", "invalid name")
+    if(!inputStatusMessage(element, nameChecker(personalName.value), "valid name", "invalid name")){
+        nameFlag = 1
+    }
+    disableSubmit()
 })
 personalPassword.addEventListener("input", () =>{
     const element = document.querySelector("#personal-password ~ p")
@@ -104,8 +133,4 @@ personalPassword.addEventListener("input", () =>{
 })
 personalPassword.addEventListener("focusout", ()=>{
     document.querySelector("form ul").classList.add("hide")
-})
-gender.addEventListener("click", ()=>{
-    const genderValue = document.querySelector(".radios input[name=gender]").value
-    console.log(genderValue);
 })
