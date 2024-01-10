7a85f08c8f53428e971b3616b0b2e4688e899cfc
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-01-10 11:38:34 +0100

    validate te password field

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ const personalPassword = document.getElementById("personal-password")
 const companyEmail = document.getElementById("company-email")
 const companyName = document.getElementById("company-name")
 const companyPassword = document.getElementById("company-password")
+const gender = document.querySelector(".radios > div > *")
+
+const capitals = document.getElementById("capitals")
+const lowers = document.getElementById("lowers")
+const number = document.getElementById("number")
+const specials = document.getElementById("specials")
+const length = document.getElementById("length")
 
 const hide = (element) =>{
     element.classList.add("hide")
@@ -50,19 +57,55 @@ const nameChecker = (name) =>{
     const nameRegex = /^[a-z]+[a-z_' -]{4,29}$/i
     return message = nameRegex.test(name) ? 1 : 0
 }
+
 const passwordChecker = (password) => {
-    const passwordRegex = /^[@a-zA-Z0-9_. ]+[@a-zA-Z0-9_. ]/
-    return message =passwordRegex.test(password) ? 1 : 0
+    const passwordCases = (element, regex) =>{
+        if(regex.test(password)){
+            element.classList.add("success")
+            element.classList.remove("failure")
+        }else{
+            element.classList.add("failure")
+            element.classList.remove("success")
+        }
+    }
+    // for upper cases
+    passwordCases(capitals, /[A-Z]/)
+
+    //for lowercases
+    passwordCases(lowers, /[a-z]/)
+
+    //for numbers
+    passwordCases(number, /[0-9]/)
+
+    // for specials
+    passwordCases(specials, /[\@\$\+\/\_\.]/)
+    // for length
+    if (password.length >= 8) {
+        length.classList.remove("failure")
+        length.classList.add("success")
+    }else{
+        length.classList.remove("success")
+        length.classList.add("failure")
+    }
 }
-personalEmail.addEventListener("keypress", (event) =>{
+personalEmail.addEventListener("input", () =>{
     const element = document.querySelector("#personal-email ~ p")
     inputStatusMessage(element, emailChecker(personalEmail.value), "valid email", "invalid email")
 })
-personalName.addEventListener("keypress", (event) =>{
+personalName.addEventListener("input", () =>{
+    //event.preventDefault()
     const element = document.querySelector("#personal-name ~ p")
     inputStatusMessage(element, nameChecker(personalName.value), "valid name", "invalid name")
 })
-personalPassword.addEventListener("keypress", (event) =>{
+personalPassword.addEventListener("input", () =>{
     const element = document.querySelector("#personal-password ~ p")
-    inputStatusMessage(element,passwordChecker(personalPassword.value), "valid password", "invalid password")
+    document.querySelector("form ul").classList.remove("hide")
+    passwordChecker(personalPassword.value)
+})
+personalPassword.addEventListener("focusout", ()=>{
+    document.querySelector("form ul").classList.add("hide")
+})
+gender.addEventListener("click", ()=>{
+    const genderValue = document.querySelector(".radios input[name=gender]").value
+    console.log(genderValue);
 })
